10bc6ccf1094c4fe72868ddb071e88de5e98efbf
venetrius <gergelygjuhasz@gmail.com>
2023-11-04 13:05:52 +0100

    BE add project controller and endPoint

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(Request $request)
+    {
+        $projects = Project::all();
+        return response()->json($projects);
+    }
+}
