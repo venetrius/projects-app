97554648c8dc3f5107f880c3475a43263c6755e6
venetrius <gergelygjuhasz@gmail.com>
2023-11-04 13:33:18 +0100

    FE Add project type

---
+++ frontend/src/types.ts
@@ -0,0 +1,10 @@
+export type Project = {
+    id: number;
+    name: string;
+    description: string;
+    created_at: string;
+    updated_at: string;
+    expected_length: string;
+    technologies: string[];
+    is_active: boolean;
+}
