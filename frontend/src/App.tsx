3a21f21528a44a55c6b7ad5e1a26b79a4f030ec5
venetrius <gergelygjuhasz@gmail.com>
2023-11-05 09:17:03 +0100

    FE move context provider to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import Home from './Pages/Home';
+import { ProjectListProvider } from './context/projectList';
 
 function App() {
   return (
-    <div className="App"> 
+    <ProjectListProvider>
       <Home />
-    </div>
+    </ProjectListProvider>
   );
 }
 
--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ProjectTable from '../../Components/Project/Table/Index';
-import { ProjectListProvider } from '../../context/projectList';
 
 const Home: React.FC = () => {
-    return <ProjectListProvider><ProjectTable /></ProjectListProvider>;
+    return <ProjectTable />;
 };
 
 export default Home;
