9edf25807f7942d5ca2449010ca2d7fe0c5e2637
venetrius <gergelygjuhasz@gmail.com>
2023-11-05 09:24:08 +0100

    FE add route and placeholder component for newProject page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import { ProjectListProvider } from './context/projectList';
+import NewProject from './Pages/NewProject';
 
 function App() {
   return (
     <ProjectListProvider>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/projects/new" element={<NewProject />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </Router>
+
     </ProjectListProvider>
   );
 }
---
+++ frontend/src/Pages/NewProject/index.tsx
@@ -0,0 +1,7 @@
+interface NewProjectProps {}
+
+const NewProject: React.FC<NewProjectProps> = ({ }) => {
+    return <h2>Work in progress</h2>
+};
+
+export default NewProject;
