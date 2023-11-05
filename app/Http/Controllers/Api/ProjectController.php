aa24c698c2f973f63e216aad4d7d724715187353
venetrius <gergelygjuhasz@gmail.com>
2023-11-05 11:14:47 +0100

    BE add validation to create project

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -19,15 +19,26 @@ class ProjectController extends Controller
     {
         Log::debug("Handling create project request");
         Log::debug($request->all());
+
+        $rules = [
+            'name' => 'required|max:255|unique:projects',
+            'description' => 'required|max:2048',
+            'technologies' => 'required|array',
+            'expected_length' => 'required|min:3|max:255',
+        ];
+        
+        LOG::debug("Validating request data");
+        $validatedData = $request->validate($rules);
+
         $defaultValues = [
             'isActive' => true,
             'user_id' => 1, // TODO should read user from context after auth is implemented
         ];
-        
-        $dataToStore = array_merge($defaultValues, $request->all());
-        Log::debug("Merged default values with request data");
+
+        Log::debug("Merging default values with validated request data");
+        $dataToStore = array_merge($defaultValues, $validatedData);
+
         Log::debug("Creating project");
-        
         $project = Project::create($dataToStore);
         Log::debug("Project created");
 
